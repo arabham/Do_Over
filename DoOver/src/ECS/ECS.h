21289695ba1db847c20090df8b032447546f6657
Abraham <arabham110@gmail.com>
2022-11-17 19:35:12 -0600

    building ECS

---
+++ DoOver/src/ECS/ECS.h
@@ -0,0 +1,56 @@
+#ifndef ECS_H
+#define ECS_H
+
+#include <bitset>
+#include <vector>
+
+const usigned int MAX_COMPONENTS = 32;
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// Signature
+/////////////////////////////////////////////////////////////////////////////////////////////
+// We use a bitset (1s and 0s) to keep track of which components an entity has,
+// and also helps keep track of which entities a system is interested in.
+/////////////////////////////////////////////////////////////////////////////////////////////
+typedef std::bitset<MAX_COMPONENTS> Signature;
+
+class Component
+{
+};
+
+class Entity
+{
+public:
+    Entity(int id) : id(id){};
+    int GetId() const;
+
+private:
+    int id;
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+// System
+/////////////////////////////////////////////////////////////////////////////////////////////
+// The system processes entities that contain a specific signature
+/////////////////////////////////////////////////////////////////////////////////////////////
+class System
+{
+public:
+    System() = default;
+    ~System() = default;
+
+    void AddEntityToSystem(Entity entity);
+    void RemoveEntityFromSystem(Entity entity);
+    std::vector<Entity> GetSystemEntities() const;
+    Signature &GetComponentSignature() const;
+
+private:
+    Signature componentSignature;
+    std::vector<Entity> entities;
+};
+
+class Registry
+{
+};
+
+#endif
